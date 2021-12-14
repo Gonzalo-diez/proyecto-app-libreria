06bc17350593cea2453768021e97b657abad8551
gonzalo.diez <gonzalodiez97@gmail.com>
2021-12-14 15:00:06 -0300

    arreglos

--- src/CartWidget.js
+++ src/CartWidget.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Component } from "react/cjs/react.development";
 
-function CartWidget() {
+class CartWidget extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
     return (
         <div className="icons">
             <FontAwesomeIcon icon={faHeart} />
-            <FontAwesomeIcon icon={faShoppingCart} />
+            <button><FontAwesomeIcon icon={faShoppingCart} /></button>
             <FontAwesomeIcon icon={faUser} />
         </div>
     )
+    }
 }
 
 export default CartWidget
