c22028174d405d0a4af8169619090b35c4a68ba9
Gonzalo-diez <86299708+Gonzalo-diez@users.noreply.github.com>
2022-01-17 13:57:35 -0300

    Arreglos

--- src/Components/Carrito/Cart.js
+++ src/Components/Carrito/Cart.js
@@ -1,18 +1,23 @@
 import { useContext } from "react"
 import { CartContext } from "./CartContext"
+import { Link } from "react-router-dom"
 
 const Cart = () => {
 
-    const { cantidad_total,  } = useContext(CartContext)
+    const { carrito, deleteItem } = useContext(CartContext)
 
-    return (
-        <div>
-            <h2>Carrito</h2>
-            <p>Cantidad Total : {cantidad_total}</p>
-            <p>Producto: </p>
-            <button>Quitar</button>
-        </div>
-    )
+     
+ ​  ​return​ ​( 
+ ​    ​<​div​> 
+ ​      ​{​carrito​.​length​ ​===​ ​0​ ​&& 
+ ​        ​<​div​> 
+ ​          ​<​p​>​No hay productos aún​<​/​p​> 
+ ​          ​<​Link​ ​to​=​'/'​>​Ir al inicio​<​/​Link​> 
+ ​        ​<​/​div​> 
+ ​      ​} 
+ ​      ​{​(​carrito​.​length​ ​>​ ​0​)​ ​&&​ ​carrito​.​map​(​prod​ ​=>​ ​<​CartItem​ ​key​=​{​prod​.​item​.​id​}​ ​product​=​{​prod​}​ ​deleteItem​=​{​deleteItem​}​ ​/​>​)​} 
+ ​    ​<​/​div​> 
+ ​  ​)
 }
 
 export default Cart
